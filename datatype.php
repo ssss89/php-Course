5d6760d552db82678ba7cb8476106f10db3546f8
ssss89 <115527387+ssss89@users.noreply.github.com>
2022-12-28 23:29:25 +0530

    PHP DATATYPES

---
+++ datatype.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>
+		PHP Datatypes
+	</h1>
+
+	<h3>
+		<?php 
+
+$a = array("css",2,"2","20.29",20.40);
+var_dump($a);
+
+?>
+	</h3>
+</body>
+</html>
