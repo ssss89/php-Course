ded016ca7e2ce9d22349637f4d3c56fb955f34ac
ssss89 <115527387+ssss89@users.noreply.github.com>
2022-12-29 00:22:57 +0530

    php variable

---
+++ variable.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>
+		PHP Variable
+		
+	</h1>
+
+	<h3> How to Right
+	<ul>
+		<li>$a</li>
+		<li>$_a</li>
+		<li>$a_1</li>
+		<li>$a1</li>
+		<li>$AbcDance</li>
+	</ul>
+	</h3>
+
+	<h4>
+		<?php
+
+		define(num,"SHiv",true);
+
+		echo Num . '<br>' ;
+
+		define(num,"s");
+echo num;
+
+
+$a = "hello";
+		$_a = "hey";
+		$_1 = "tata";
+		$a1 = "bata";
+
+		$UserName = "sata";
+
+
+
+
+		echo $UserName . '<br>' ;
+
+
+		function a(){
+			$asd  = "hello function";
+
+			echo $asd;
+		}
+
+		a();
+
+		
+
+?>
+	</h4>
+</body>
+</html>
